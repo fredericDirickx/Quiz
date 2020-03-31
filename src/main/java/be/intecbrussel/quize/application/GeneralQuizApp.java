b2165146943cde44b3be1ca8101c758f9c3a5d54
FREDERICD <frederic.dirckx@gmail.com>
2020-03-31 10:16:59 +0200

    still generics

--- src/main/java/be/intecbrussel/quize/application/GeneralQuizApp.java
+++ src/main/java/be/intecbrussel/quize/application/GeneralQuizApp.java
@@ -3,24 +3,19 @@ package be.intecbrussel.quize.application;
 import be.intecbrussel.quize.implementation.NumberGenerator;
 import be.intecbrussel.quize.implementation.QuizService;
 
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class GeneralQuizApp {
     public static void main(String[] args) {
 
 
-        NumberGenerator<Number> numberGenerator = new NumberGenerator<Number>(7.2,8.35,0.0,11.0);
+        NumberGenerator<Number> numberGenerator = new NumberGenerator<Number>(8, 9, 0, 11);
         QuizService<Number> quizService = null;
-            quizService = new QuizService<Number>(5,false,false,false,false,numberGenerator);
+        quizService = new QuizService<Number>(100, false, false, true, false, numberGenerator);
 
 
         Scanner input = new Scanner(System.in);
         String enGame = "y";
-
 
 
         while (enGame.equals("y")) {
@@ -32,11 +27,9 @@ public class GeneralQuizApp {
 
             System.out.println("Do you want to play again? y or n ?");
             enGame = input.next();
-
         }
 
         System.out.println("SEE YOU NEXT TIME!!!!!");
-
 
 
     }
