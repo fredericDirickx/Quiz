849ded3f0c25007ecc7c0d25f983b1e85606502c
FREDERICD <frederic.dirckx@gmail.com>
2020-09-26 17:02:22 +0200

    arithmetics with a 'e'

--- src/main/java/tech/dirickx/littlearithmetics/LittlearithmeticsApplication.java
+++ src/main/java/tech/dirickx/littlearithmetics/LittlearithmeticsApplication.java
@@ -2,12 +2,13 @@ package tech.dirickx.littlearithmetics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = tech.dirickx.littlearithmetics.repositories.UserRepository.class)
-public class LittlearithmeticsApplication {
+public class LittlearithmeticsApplication extends SpringBootServletInitializer {
     public static void main(String[] args){
         ApplicationContext applicationContext = SpringApplication.run(LittlearithmeticsApplication.class, args);
     }
